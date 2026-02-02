97eda068e28a102edc65e67dc3fcd726f1b73251
Itayra <likeosik@gmail.com>
2026-02-02 17:26:10 +0200

    Initial setup of the project scructure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Реалізація відкриття/закриття меню
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock'); // Блокуємо скрол
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('active');
+    });
+}
